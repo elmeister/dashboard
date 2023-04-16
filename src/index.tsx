0c0a9a4257ea916b6a31cc67ab60d66ab7e73661
elmeister <elegirmeister@gmail.com>
2023-04-16 16:27:09 +0700

    Make exlint semi happy

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from 'pages/App';
+import App from 'pages/App'
 
-import './index.css';
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.querySelector('#root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
